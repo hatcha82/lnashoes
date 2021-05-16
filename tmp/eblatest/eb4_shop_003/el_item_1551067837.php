42bd68d2625980123ed9c23f818e5ace34a8ff40
root <hatcha82@gmail.com>
2021-05-16 03:04:56 +0000

    aa

--- tmp/eblatest/eb4_shop_003/el_item_1551067837.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!defined('_EYOOM_')) exit;
-$el_item = array(
-	"0" => array("li_no" => "100","el_code" => "1551067837","li_theme" => "eb4_shop_003","li_state" => "1","li_sort" => "1","li_title" => "Main Photo","li_link" => "","li_target" => "_self","li_bo_table" => "magazine_entertain","li_gr_id" => "","li_exclude" => "","li_include" => "","li_tables" => "magazine_entertain","li_direct" => "n","li_count" => "9","li_depart" => "2","li_period" => "0","li_type" => "w","li_ca_view" => "n","li_cut_subject" => "0","li_best" => "n","li_random" => "n","li_img_view" => "y","li_img_width" => "600","li_img_height" => "0","li_content" => "n","li_cut_content" => "0","li_bo_subject" => "n","li_mbname_view" => "n","li_photo" => "n","li_use_date" => "n","li_date_type" => "2","li_date_kind" => "Y-m-d H:i:s","li_view_level" => "1","li_regdt" => "2019-02-25 13:12:27"),
-);
--- tmp/ebslider/eb4_shop_003/es_item_1548224900.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('_EYOOM_')) exit;
-$es_item = array(
-	"0" => array("ei_no" => "60","es_code" => "1548224900","ei_theme" => "eb4_shop_003","ei_state" => "1","ei_sort" => "1","ei_title" => "NEW ARRIVALS","ei_subtitle" => "나만의 스타일 나만의 개성을 가질 수 있는 신제품을 바로 만나보세요.","ei_text" => "","ei_link" => "a:1:{i:0;s:24:\"http://theme4.eyoom.net/\";}","ei_target" => "a:1:{i:0;s:5:\"_self\";}","ei_img" => "a:1:{i:0;s:36:\"1817c6cb786b51d6bada58ecca7f7bca.jpg\";}","ei_period" => "1","ei_start" => "","ei_end" => "","ei_clicked" => "0","ei_view_level" => "1","ei_regdt" => "2019-02-22 13:37:01"),
-	"1" => array("ei_no" => "61","es_code" => "1548224900","ei_theme" => "eb4_shop_003","ei_state" => "1","ei_sort" => "2","ei_title" => "NEW SEASON","ei_subtitle" => "따뜻한 감성의 계절에 드디어 에가뤼시 포가누 콜렉션이 다가옵니다.","ei_text" => "","ei_link" => "a:1:{i:0;s:24:\"http://theme4.eyoom.net/\";}","ei_target" => "a:1:{i:0;s:5:\"_self\";}","ei_img" => "a:1:{i:0;s:36:\"71a7dc6ac69c526bc9305211cb82a53d.jpg\";}","ei_period" => "1","ei_start" => "","ei_end" => "","ei_clicked" => "0","ei_view_level" => "1","ei_regdt" => "2019-02-22 13:37:23"),
-);

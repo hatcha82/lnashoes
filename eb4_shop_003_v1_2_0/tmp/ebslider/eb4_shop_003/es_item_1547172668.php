ac36b2fbe950793d2ec23e31eb006886dd9efdd8
root <hatcha82@gmail.com>
2021-05-16 03:03:22 +0000

    tt

--- eb4_shop_003_v1_2_0/tmp/ebslider/eb4_shop_003/es_item_1547172668.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('_EYOOM_')) exit;
-$es_item = array(
-	"0" => array("ei_no" => "62","es_code" => "1547172668","ei_theme" => "eb4_shop_003","ei_state" => "1","ei_sort" => "1","ei_title" => "슬라이드01","ei_subtitle" => "","ei_text" => "","ei_link" => "a:1:{i:0;s:23:\"http://theme4.eyoom.net\";}","ei_target" => "a:1:{i:0;s:5:\"_self\";}","ei_img" => "a:2:{i:0;s:36:\"dc253d0f09ea1a430951ae58cd7766ed.jpg\";i:1;s:36:\"ac4335e666f07d07b22cd436421dbf4d.jpg\";}","ei_period" => "1","ei_start" => "","ei_end" => "","ei_clicked" => "0","ei_view_level" => "1","ei_regdt" => "2019-02-22 15:49:43"),
-	"1" => array("ei_no" => "63","es_code" => "1547172668","ei_theme" => "eb4_shop_003","ei_state" => "1","ei_sort" => "2","ei_title" => "슬라이드02","ei_subtitle" => "","ei_text" => "","ei_link" => "a:1:{i:0;s:23:\"http://theme4.eyoom.net\";}","ei_target" => "a:1:{i:0;s:5:\"_self\";}","ei_img" => "a:2:{i:0;s:36:\"ee7f94badbc68c09403603cc93ba1660.jpg\";i:1;s:36:\"75495c82bc9afc7bedd2c58c4fa475fb.jpg\";}","ei_period" => "1","ei_start" => "","ei_end" => "","ei_clicked" => "0","ei_view_level" => "1","ei_regdt" => "2019-02-22 15:52:05"),
-);

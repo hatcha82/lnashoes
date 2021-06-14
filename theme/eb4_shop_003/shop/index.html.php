a63b0ae14b132c3957100e78681c20beaef35be9
CHANGBEOM HA <hatcha82@gmail.com>
2021-06-14 15:16:27 +0900

    youtube add / 이벤트 없음 제거

--- theme/eb4_shop_003/shop/index.html.php
+++ theme/eb4_shop_003/shop/index.html.php
@@ -67,20 +67,7 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fcfff4', end
     </div>
 </section>
 
-<?php    
-    echo "<div class='lt list_01' >
-            <div class='bo_name'>
-                <div style='width:100%;padding:0px;margin:0 auto;'>
-                <div id='youtube_area' style='width:100%;border:1px solid #444'>
-                    <div style='position: relative; padding-bottom: 56.25%;'>
-                    <iframe style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' 
-                        src='https://www.youtube.com/embed/ja6dKMqgPRE?autoplay=0&playsinline=1'
-                        frameborder='0' gesture='media' allow='autoplay;encrypted-media' allowfullscreen='allowfullscreen'></iframe
-                    </div>
-                </div>
-            </div> 
-        </div>"
-?>
+
 
 
 <?php /* ---------- EB Contents 스킨 - Brand By Categories ---------- */ ?>
@@ -160,6 +147,21 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fcfff4', end
         <?php } ?>
     </div>
 </section>
+
+<?php    
+    echo "<div class='lt list_01' >
+            <div class='bo_name'>
+                <div style='width:100%;padding:0px;margin:0 auto;'>
+                <div id='youtube_area' style='width:100%;border:1px solid #444'>
+                    <div style='position: relative; padding-bottom: 56.25%;'>
+                    <iframe style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' 
+                        src='https://www.youtube.com/embed/ja6dKMqgPRE?autoplay=0&playsinline=1'
+                        frameborder='0' gesture='media' allow='autoplay;encrypted-media' allowfullscreen='allowfullscreen'></iframe
+                    </div>
+                </div>
+            </div> 
+        </div>"
+?>
 
 <?php /* ---------- 이벤트 - 쇼핑몰현황/기타 > 이벤트관리에서 상품 등록합니다. ---------- */ ?>
 <section class="section section-08 shop-section-event">

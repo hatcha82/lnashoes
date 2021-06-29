186ba9fc7ed8d4a50ab6d8fff12823bc90f7a230
CHANGBEOM HA <hatcha82@gmail.com>
2021-06-29 20:39:35 +0900

    이벤트 추가

--- theme/eb4_shop_003/shop/index.html.php
+++ theme/eb4_shop_003/shop/index.html.php
@@ -185,8 +185,6 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fcfff4', end
 </section>
 
 
-
-
 <?php /* ---------- 이벤트 - 쇼핑몰현황/기타 > 이벤트관리에서 상품 등록합니다. ---------- */ ?>
 <section class="section section-08 shop-section-event">
     <div class="container">
